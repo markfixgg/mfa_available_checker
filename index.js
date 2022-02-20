893e8c2065a784dd7d8c2c719b6b2b70cf422947
Yevhen <lebedenko077@gmail.com>
2022-02-20 16:54:06 +0300

    bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TicketsApi = require("./TicketsApi");
+const { Telegraf } = require('telegraf');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
@@ -12,5 +13,14 @@ const service = "Оформлення закордонного паспорта"
     // Initialisation
     const Api = new TicketsApi(country, consulate, serviceCategory, service);
 
-    const schedules = await Api.getSchedules();
+    const bot = new Telegraf("5139692106:AAGHArrDS5ClK-b_RKztJ5yY9a8EfChk_Us")
+
+    // bot.use((ctx, next)=>{
+    //     console.log(ctx.from);
+    //     ctx.reply('Server running :)')
+    //
+    //     return next();
+    // });
+
+    bot.launch()
 })();
