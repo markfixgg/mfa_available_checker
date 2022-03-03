dfaff273f549c21b93e0a2c5fe32908f34da76d9
markfixgg <lebedenko077@gmail.com>
2022-03-03 17:53:13 +0200

    Refactor and updates
    Create .gitignore

--- TicketsApi.js
+++ TicketsApi.js
@@ -1,8 +1,7 @@
-// Status codes
+const moment = require("moment");
 const axios = require("axios");
 const ac = require("@antiadmin/anticaptchaofficial");
 const fs = require("fs");
-const moment = require("moment");
 
 const statusCodes = {
     200: "OK",
@@ -17,6 +16,7 @@ class TicketsApi {
         this.serviceCategory = serviceCategory;
         this.service = service;
 
+        this.debug = true;
         this.siteURL = "https://online.mfa.gov.ua/api/v1/auth/session";
         this.siteKey = "6LcPNjgbAAAAAIp0KyR2RK_e7gb6ECDXR0n-JLqG";
         this.captchaKey = "a0b76abb37db80b699ae4444e11f3a27";
@@ -33,94 +33,142 @@ class TicketsApi {
     }
 
     async getCountryId() {
-        const response = (await this.instance.get('countries?order=shortName%2CASC&offset=0&limit=500'))?.data;
-        const { meta, data } = response;
+        try {
+            const response = (await this.instance.get('countries?order=shortName%2CASC&offset=0&limit=500'))?.data;
+            const { meta, data } = response;
 
-        if(meta?.items_count <= 0) {
-            throw new Error('Countries not found!');
+            if(meta?.items_count <= 0) {
+                throw new Error('Countries not found!');
+            }
+
+            const countryId = (data.find(x => x.name.includes(this.country)))?.id;
+
+            if(this.debug) {
+                console.log('[INFO]', `Country code is: ${countryId}.`)
+            }
+
+            return countryId;
+        } catch (e) {
+            return 0;
         }
-
-        const countryId = (data.find(x => x.name.includes(this.country)))?.id;
-        // console.log('[INFO]', `Country code is: ${countryId}.`)
-
-        return countryId;
     }
 
     async getConsulateId() {
-        const countryId = await this.getCountryId();
+        try {
+            const countryId = await this.getCountryId();
 
-        const response = await this.instance.get(`queue/consulates?countryId=${countryId}`).then(res => res).catch(err => err);
-        if(response?.response?.status === 401) {
-            await this.updateAuthToken();
-            return this.getConsulateId();
+            const response = await this.instance.get(`queue/consulates?countryId=${countryId}`).then(res => res).catch(err => err);
+            if(response?.response?.status === 401) {
+                await this.updateAuthToken();
+                return this.getConsulateId();
+            }
+
+            const { data } = response?.data;
+            if(!data) return null;
+
+            const consulateId = (data.find(x => x.shortName.includes(this.consulate)))?.id;
+            if(this.debug) {
+                console.log('[INFO]', `ConsulateID code is: ${consulateId}.`)
+            }
+
+            return consulateId;
+        } catch (e) {
+            return 0;
         }
-
-        const { data } = response?.data;
-        if(!data) return null;
-
-        const consulateId = (data.find(x => x.shortName.includes(this.consulate)))?.id;
-        // // console.log('[INFO]', `ConsulateID code is: ${consulateId}.`)
-
-        return consulateId;
     }
 
     async getServiceCategoryId() {
-        const consulateId = await this.getConsulateId();
+        try {
+            const consulateId = await this.getConsulateId();
 
-        const response = await this.instance.get(`queue/service-categories?limit=500&scheduledConsulateId=${consulateId}`).then(res => res).catch(err => err);
-        if(response?.response?.status === 401) {
-            await this.updateAuthToken();
-            return this.getServiceCategoryId();
+            const response = await this.instance.get(`queue/service-categories?limit=500&scheduledConsulateId=${consulateId}`).then(res => res).catch(err => err);
+            if(response?.response?.status === 401) {
+                await this.updateAuthToken();
+                return this.getServiceCategoryId();
+            }
+
+            const { data } = response?.data;
+            if(!data) return null;
+
+            const serviceCategoryId = (data.find(x => x.name.includes(this.serviceCategory)))?.id;
+            // console.log('[INFO]', `ServiceCategoryId code is: ${serviceCategoryId}.`)
+
+            return serviceCategoryId;
+        } catch (e) {
+
         }
-
-        const { data } = response?.data;
-        if(!data) return null;
-
-        const serviceCategoryId = (data.find(x => x.name.includes(this.serviceCategory)))?.id;
-        // console.log('[INFO]', `ServiceCategoryId code is: ${serviceCategoryId}.`)
-
-        return serviceCategoryId;
     }
 
+    /**
+     * Get serviceID
+     * @param scheduledOnly
+     * @returns {Promise<*|null|undefined>}
+     */
     async getServiceId(scheduledOnly = true) {
-        const consulateId = await this.getConsulateId();
-        const serviceCategoryId = await this.getServiceCategoryId();
+        try {
+            // Get consulateID
+            const consulateId = await this.getConsulateId();
 
-        const response = await this.instance.get(`queue/services?consulateId=${consulateId}&limit=500&serviceCategoryId=${serviceCategoryId}&scheduledOnly=${scheduledOnly}`).then(res => res).catch(err => err);
-        if(response?.response?.status === 401) {
-            await this.updateAuthToken();
-            return this.getServiceId();
+            // Get serviceCategoryID
+            const serviceCategoryId = await this.getServiceCategoryId();
+
+            // Request queue/services (ID)
+            const response = await this.instance.get(`queue/services?consulateId=${consulateId}&limit=500&serviceCategoryId=${serviceCategoryId}&scheduledOnly=${scheduledOnly}`).then(res => res).catch(err => err);
+
+            // If status === Unauthorized, recursive request
+            if(response?.response?.status === 401) {
+                await this.updateAuthToken();
+                return this.getServiceId();
+            }
+
+            const { data } = response?.data;
+            if(!data) return null;
+
+            const serviceId = (data.find(x => x.shortName.includes(this.service)))?.id;
+            if(this.debug) {
+                console.log('[INFO]', `ServiceId code is: ${serviceId}.`)
+            }
+
+            return serviceId;
+        } catch (e) {
+
         }
 
-        const { data } = response?.data;
-        if(!data) return null;
-
-        const serviceId = (data.find(x => x.shortName.includes(this.service)))?.id;
-        // console.log('[INFO]', `ServiceId code is: ${serviceId}.`)
-
-        return serviceId;
     }
 
+    /**
+     * Return date/time schedules
+     * @param date - start date
+     * @param dateEnd - end date
+     * @returns {Promise<unknown>}
+     */
     async getSchedules(date = moment().format('YYYY-MM-DD'), dateEnd = moment().format('YYYY-MM-DD')) {
-        const consulateId = await this.getConsulateId();
-        const serviceId = await this.getServiceId();
+        try {
+            // Get consulateID
+            const consulateId = await this.getConsulateId();
 
-        const response = await this.instance.get(`queue/consulates/${consulateId}/schedule?date=${date}&dateEnd=${dateEnd}&serviceId=${serviceId}`).then(res => res).catch(err => err);
-        if(response?.response?.status === 401) {
-            await this.updateAuthToken();
-            return this.getSchedules();
+            // Get serviceID
+            const serviceId = await this.getServiceId();
+
+            // Request schedules
+            const response = await this.instance.get(`queue/consulates/${consulateId}/schedule?date=${date}&dateEnd=${dateEnd}&serviceId=${serviceId}`).then(res => res).catch(err => err);
+
+            // If status === Unauthorized, recursive request
+            if(response?.response?.status === 401) {
+                await this.updateAuthToken();
+                return this.getSchedules();
+            }
+
+            return response;
+        } catch (e) {
+            return { success: false, error: e.message }
         }
-
-        if(Array.isArray(response.data) && response?.data?.length === 0) {
-            console.log("[INFO]", "Empty tickets list.");
-            return null;
-        }
-
-        return response;
     }
 
+    /**
+     * Load saved auth_token (JWT) from file
+     */
     loadSavedToken () {
-        // Load saved token
         try {
             let file = fs.readFileSync('./auth_token.json');
             let json = JSON.parse(file || "");
@@ -132,21 +180,31 @@ class TicketsApi {
         } catch (e) {}
     }
 
+    /**
+     * Request new auth token from site
+     * @returns {Promise<void>}
+     */
     async updateAuthToken() {
         try {
             if(this.fetchingToken) return;
             this.fetchingToken = true;
 
+            // Get country ID
             const countryId = await this.getCountryId();
+
+            // Request solved captcha GResponse
             const gresponse = await ac.solveRecaptchaV2Proxyless(this.siteURL, this.siteKey)
 
+            // Request session JWT token
             const { token } = (await this.instance.post(`auth/session`, { "g-recaptcha-response": gresponse, countryId }).then(res => res).catch(err => err))?.data;
 
+            // Save JWT to file
             fs.writeFileSync('./auth_token.json', JSON.stringify({
                 auth_token: token,
                 create_date: Date.now()
             }, null, 2));
 
+            // Change defaults headers in axios instance
             this.instance.defaults.headers.Authorization = `Bearer ${token}`;
         } catch (e) {
             console.log(e.message)
